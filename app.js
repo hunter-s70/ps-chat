22a8b1d1900fb240d1618e01fb88ff234dfb21e3
hunter-s70 <serg.buhtoyarov@gmail.com>
2018-09-10 20:46:47 +0400

    add login page UI

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var MongoStore = require('connect-mongo')(session); // to save session in DB
 // routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -38,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
