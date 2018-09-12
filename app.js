91a9891d6daaa633a09d3917c43719b4bc6e7bb2
hunter-s70 <serg.buhtoyarov@gmail.com>
2018-09-12 21:55:49 +0400

    add version 1 user registration, send ajax, redirect to chat, check password, save session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 var HttpError = require('error').HttpError;
 
@@ -15,6 +16,7 @@ var MongoStore = require('connect-mongo')(session); // to save session in DB
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
+var chatRouter = require('./routes/chat');
 
 var app = express();
 
@@ -26,6 +28,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser());
 app.use(cookieParser());
 app.use(session({
   secret: config.get('session:secret'),
@@ -40,6 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/chat', chatRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,42 @@
 var express = require('express');
+var User = require('models/user').User;
+var HttpError = require('error').HttpError;
+var async = require('async');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('pages/login', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  async.waterfall([
+    function(callback) {
+      User.findOne({ username: username }, callback);
+    },
+    function(user, callback) {
+      if (user) {
+        if (user.checkPassword(password)) {
+          callback(null, user);
+        } else {
+          next(new HttpError(403, "Incorrect password"));
+        }
+      } else {
+        var user = new User({username: username, password: password});
+        user.save(function(err) {
+          if (err) return next(err);
+          callback(null, user);
+        });
+      }
+    }
+  ], function(err, user) {
+    if (err) return next(err);
+    req.session.user = user._id;
+    res.send({});
+  });
 });
 
 module.exports = router;
