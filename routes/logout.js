2b25956cef58ec705af7334dfa762c10763b3232
hunter-s70 <serg.buhtoyarov@gmail.com>
2018-10-13 20:07:40 +0400

    fix chat markup, small fixes

--- routes/logout.js
+++ routes/logout.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-  var sid = req.session.id;
-  var io = req.app.get('io');
-
+  // var sid = req.session.id;
+  // var io = req.app.get('io');
   req.session.destroy(function(err) {
     // io.sockets.emit('session:reload', sid);
     if (err) return next(err);
